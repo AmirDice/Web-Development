af1afc0afb258251e4017ef9207a590f46807ff0
Amir <inuwaamir@gmail.com>
2022-03-13 19:15:13 +0100

    ADD: TodoForm component

---
+++ react-todolist-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoForm />
+    </div>
+  );
+}
+
+export default App;
